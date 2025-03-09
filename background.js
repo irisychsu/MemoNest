cd74755fb765e6904e3e6dab898df9c81b1e62b3
iris <iris@Mac.home>
2025-03-09 13:05:36 +0800

    修正 background.js，合併 onMessage 監聽器

--- background.js
+++ background.js
@@ -7,4 +7,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         });
         return true; // 讓 Chrome 延遲回應，等待 API 結果
     }
+
+    if (request.action === "getPageInfo") {
+        sendResponse({
+            title: sender.tab.title,
+            url: sender.tab.url
+        });
+    }
 });
